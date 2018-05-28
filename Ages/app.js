8d93d1604260cdde547ce1aa5bdf8eec18c4b5f5
Jessica Alejandra Avila Vallejos <javila.vallejos@gmail.com>
2018-05-28 04:38:05 -0300

    se guardan todas las estructuras condicionales

--- Ages/app.js
+++ Ages/app.js
@@ -6,3 +6,28 @@ const age = prompt("¿Cuál es tu edad?");
 if (age<1){
 alert ("Ingresa un número mayor que cero!!")
 }
+else if (age%1 !=0){
+    alert("Ingresa un Numero entero");
+    }
+/** mensaje de error si se ingresa un decimal */   
+ 
+    else if (age>21){
+    alert("Wow eres todo un adulto!!");
+    }
+        else if (age >18 && age <=21) {
+        alert("Eres un Young Adult!!");      
+        } 
+            else if (age >12 && age <=18) {
+            alert("Eres un Teenager!!");     
+            }
+                else if(age >5 && age <=12)
+                {
+                alert("Eres un Grandeschooler!!"); 
+                }
+                    else if(age >3 && age <=5)
+                    {
+                    alert("Eres un Preschooler!!"); 
+                    }
+                        else if(age >=1 && age <=3) {
+                        alert("Eres un Toddler!!");    
+                        }
