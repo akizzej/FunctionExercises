5bc983125c14649db31b447386d3c549fb70aff3
Jessica Alejandra Avila Vallejos <javila.vallejos@gmail.com>
2018-05-28 05:02:55 -0300

    se imprime en web mediante document write el resultado solicitado

--- Functions/app.js
+++ Functions/app.js
@@ -1,21 +1,23 @@
 /* creamos la funcion add */
-const add = function (numero1,numero2){
-    let Numero1 = numero1;
-    let Numero2 = numero2;
+const add = function (argument1,argument2){
+    let Number1 = argument1;
+    let Number2 = argument2;
 
-    return Numero1 + Numero2
+    return Number1 + Number2
 }
 /* realizo document.write para verificar funcion add en pantalla */
 /*document.write(add(1,5));*/
 
 /**creamos la funcion multiply */
-const add = function (numero1,numero2){
-    let Numero1 = numero1;
-    let Numero2 = numero2;
+const multiply = function (argument1,argument2){
+    let Number1 = argument1;
+    let Number2 = argument2;
 
-    return Numero1 * Numero2
+    return Number1 * Number2
 }
 /* realizo document.write para verificar funcion multiply en pantalla */
 /*document.write(multiply(2,5));*/
 
 /*procedo a resolver 36325 * (9824 + 777) hago document.write para imprimir resultado en web*/
+
+document.write (multiply(36325,(add(9824,777))));
