d5cd401ba59ecce1150fbd7d9ada03c4b2b088a0
Jessica Alejandra Avila Vallejos <javila.vallejos@gmail.com>
2018-05-28 04:45:45 -0300

    se crea la funcion multiply

--- Functions/app.js
+++ Functions/app.js
@@ -7,3 +7,13 @@ const add = function (numero1,numero2){
 }
 /* realizo document.write para verificar funcion add en pantalla */
 /*document.write(add(1,5));*/
+
+/**creamos la funcion multiply */
+const add = function (numero1,numero2){
+    let Numero1 = numero1;
+    let Numero2 = numero2;
+
+    return Numero1 * Numero2
+/* realizo document.write para verificar funcion multiply en pantalla */
+/*document.write(multiply(2,5));*/
+    
